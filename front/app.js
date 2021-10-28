f876255b6122e0277d701e090b3cb528d823a9c5
Romain Dreidemy <dreidemyromain@gmail.com>
2021-10-28 21:15:23 +0200

    init front reactjs + webpack

---
+++ front/app.js
@@ -0,0 +1,5 @@
+import React from "react";
+import { render } from "react-dom";
+import Application from "./src/components/Application";
+
+render(<Application />, document.getElementById('react_root'));
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const Encore = require('@symfony/webpack-encore');
+
+if (!Encore.isRuntimeEnvironmentConfigured()) {
+    Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
+}
+
+Encore
+    // directory where compiled assets will be stored
+    .setOutputPath('public/build/')
+    // public path used by the web server to access the output path
+    .setPublicPath('/build')
+
+    /*
+     * ENTRY CONFIG
+     *
+     * Add 1 entry for each "page" of your app
+     * (including one that's included on every page - e.g. "app")
+     *
+     * Each entry will result in one JavaScript file (e.g. app.js)
+     * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
+     */
+    .addEntry('app', './front/app.js')
+
+    // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
+    .splitEntryChunks()
+
+    .enableSingleRuntimeChunk()
+    .cleanupOutputBeforeBuild()
+    .enableBuildNotifications()
+    .enableSourceMaps(!Encore.isProduction())
+    .enableVersioning(Encore.isProduction())
+    .configureBabelPresetEnv((config) => {
+        config.useBuiltIns = 'usage';
+        config.corejs = 3;
+    })
+    .enableReactPreset()
+;
+
+module.exports = Encore.getWebpackConfig();
