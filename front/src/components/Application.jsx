b2fa877b08590287a008dbb62f34e8d3eb26a650
Romain Dreidemy <dreidemyromain@gmail.com>
2021-10-28 21:34:32 +0200

    init eslint configuration

--- front/src/components/Application.jsx
+++ front/src/components/Application.jsx
@@ -1,7 +1,5 @@
-import React from "react";
+import React from 'react';
 
-const Application = () => {
-    return <h1>Application root</h1>;
-}
+const Application = () => <h1>Application root</h1>;
 
 export default Application;
