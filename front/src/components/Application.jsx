fe1e068d3cf61fb6e05d1c1ab469a3cfa2bdf054
Romain Dreidemy <dreidemyromain@gmail.com>
2021-10-28 21:54:38 +0200

    init tailwindcss

--- front/src/components/Application.jsx
+++ front/src/components/Application.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
-const Application = () => <h1>Application root</h1>;
+const Application = () => <h1 className="bg-red-300">Application root</h1>;
 
 export default Application;
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,17 @@
 const Encore = require('@symfony/webpack-encore');
+const path = require('path');
 
 if (!Encore.isRuntimeEnvironmentConfigured()) {
-    Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
+  Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
 }
 
 Encore
-    // directory where compiled assets will be stored
-    .setOutputPath('public/build/')
-    // public path used by the web server to access the output path
-    .setPublicPath('/build')
+// directory where compiled assets will be stored
+  .setOutputPath('public/build/')
+// public path used by the web server to access the output path
+  .setPublicPath('/build')
 
-    /*
+/*
      * ENTRY CONFIG
      *
      * Add 1 entry for each "page" of your app
@@ -19,21 +20,26 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
-    .addEntry('app', './front/app.js')
+  .addEntry('app', './front/app.js')
+  .addStyleEntry('tailwind', './front/src/assets/styles/tailwind.css')
 
-    // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
-    .splitEntryChunks()
+// When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
+  .splitEntryChunks()
 
-    .enableSingleRuntimeChunk()
-    .cleanupOutputBeforeBuild()
-    .enableBuildNotifications()
-    .enableSourceMaps(!Encore.isProduction())
-    .enableVersioning(Encore.isProduction())
-    .configureBabelPresetEnv((config) => {
-        config.useBuiltIns = 'usage';
-        config.corejs = 3;
-    })
-    .enableReactPreset()
-;
-
+  .enableSingleRuntimeChunk()
+  .cleanupOutputBeforeBuild()
+  .enableBuildNotifications()
+  .enableSourceMaps(!Encore.isProduction())
+  .enableVersioning(Encore.isProduction())
+  .configureBabelPresetEnv((config) => {
+    config.useBuiltIns = 'usage';
+    config.corejs = 3;
+  })
+  .enablePostCssLoader((options) => {
+    options.postcssOptions = {
+      // the directory where the postcss.config.js file is stored
+      config: path.resolve(__dirname, '', 'postcss.config.js'),
+    };
+  })
+  .enableReactPreset();
 module.exports = Encore.getWebpackConfig();
